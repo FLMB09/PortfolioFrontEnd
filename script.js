c3c9562f7f92e029709508cd727d6f880887195a
FLMB09 <118026516+FLMB09@users.noreply.github.com>
2022-12-17 17:37:02 -0300

    Skills
    Agregue Bootstrap pero no se si funciona bien, agregue section skills

--- script.js
+++ script.js
@@ -8,34 +8,35 @@ function mostrarOcultarMenu(){
         document.getElementById("nav").classList ="responsive";
         menuVisible = true;
     }
-}
+};
 
 function seleccionar(){
     //oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
     menuVisible = false;
-}
-//Funcion que aplica las animaciones de las habilidades
-function efectoHabilidades(){
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+};
+
+
+function cambiar_parrafo(){
+    document.getElementById("edit-quiensoy").style.display="block";
+    let texto = document.getElementById("texto-quiensoy").innerText;
+    console.log(texto);
+};
+
+function myFunction2(valor){
+    document.getElementById("texto-quiensoy").innerText=valor;
+};
+
+/*function typeWriter() {
+    var i=0;
+    var txt= "#YoProgramo";
+    var speed= 30;
+
+    if (i < txt.length) {
+      document.getElementById("logotyping").innerHTML += txt.charAt(i);
+      i++;
+      setTimeout(typeWriter, speed);
     }
-}
+ }*/
 
-
-//detecto el scrolling para aplicar la animacion de la barra de habilidades
-window.onscroll = function(){
-    efectoHabilidades();
-} 
+ 
